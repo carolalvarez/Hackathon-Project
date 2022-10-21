308b80103a7381efa589c2cc308ee1482cec243f
esne <informatica@esne.es>
2022-10-21 17:00:53 +0200

    ddd

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,6 +8,7 @@ public class GameManager : MonoBehaviour
     public int gold;
     public Text goldDisplay;
 
+    private int contadorBuilding;
     private Building buildingToPlace;
     public GameObject grid;
     public CustomCursor customCursor;
@@ -21,12 +22,16 @@ public class GameManager : MonoBehaviour
 
     public void BuyBuilding(Building building)
     {
-        if (gold >= building.cost)
+        if (gold >= building.cost && contadorBuilding <= 8)
         {
             customCursor.gameObject.SetActive(true);
+            customCursor.GetComponent<SpriteRenderer>().sprite = building.GetComponent<SpriteRenderer>().sprite;
+            Cursor.visible = false;
+
             gold -= building.cost;
             buildingToPlace = building;
             grid.SetActive(true);
+            contadorBuilding++;
         }
     }
 }
