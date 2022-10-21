5e12fd9f0aa62f21f31df32456db1afb902c36a5
esne <informatica@esne.es>
2022-10-21 17:16:48 +0200

    Update Building.cs

--- Assets/Scripts/Building.cs
+++ Assets/Scripts/Building.cs
@@ -5,15 +5,19 @@ using UnityEngine;
 public class Building : MonoBehaviour
 {
     public int cost;
+    public int goldIncrease;
+    public float timeBtwIncreases;
+    private float nextIncreaseTime;
+    private GameManager gm;
     // Start is called before the first frame update
     void Start()
     {
-        
+        gm = FindObjectOfType<GameManager>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Time.time > nextIncreaseTime) { nextIncreaseTime = Time.time * timeBtwIncreases; }
     }
 }
